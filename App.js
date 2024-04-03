d9d12c3cb13c41213e1e88c671e9db02f63faf38
jessieeeovo <jessieyao0602@gmail.com>
2024-04-03 16:02:03 -0400

    update homepage style

--- App.js
+++ App.js
@@ -2,16 +2,22 @@ import { StatusBar } from "expo-status-bar";
 import { StyleSheet, Text, View, Stack } from "react-native";
 import HomePage from "./src/screens/HomePage.js";
 import RecipePage from "./src/screens/RecipePage.js";
-import { NavigationContainer } from '@react-navigation/native';
-import { createNativeStackNavigator } from '@react-navigation/native-stack';
-
+import { NavigationContainer, DefaultTheme } from "@react-navigation/native";
+import { createNativeStackNavigator } from "@react-navigation/native-stack";
 
 export default function App() {
   const Stack = createNativeStackNavigator();
 
   return (
     <NavigationContainer>
-      <Stack.Navigator initialRouteName="Home">
+      <Stack.Navigator
+        initialRouteName="Home"
+        screenOptions={{
+          contentStyle: { backgroundColor: "#FFFCF2" },
+          headerShown: false,
+        }}
+        
+      >
         <Stack.Screen name="HomePage" component={HomePage} />
         <Stack.Screen name="RecipePage" component={RecipePage} />
       </Stack.Navigator>
--- src/screens/HomePage.js
+++ src/screens/HomePage.js
@@ -16,59 +16,76 @@ import {
   KaiseiDecol_700Bold,
 } from "@expo-google-fonts/kaisei-decol";
 import { setCustomText } from "react-native-global-props";
-import { NavigationContainer } from '@react-navigation/native';
-import { createNativeStackNavigator } from '@react-navigation/native-stack';
+import { NavigationContainer } from "@react-navigation/native";
+import { createNativeStackNavigator } from "@react-navigation/native-stack";
+import FontAwesome from "react-native-vector-icons/FontAwesome";
 
 function HomePage({ navigation }) {
-    let [fontsLoaded] = useFonts({
+  let [fontsLoaded] = useFonts({
     KaiseiDecol_400Regular,
     KaiseiDecol_500Medium,
     KaiseiDecol_700Bold,
   });
   setCustomText(customTextProps);
   const handleConvertPress = () => {
-    navigation.navigate('RecipePage');
+    navigation.navigate("RecipePage");
   };
   if (!fontsLoaded) {
     return;
   } else {
     return (
-      <ScrollView style={styles.container}>
+      <View style={styles.container}>
         <View style={styles.header}>
           <Text style={styles.headerText}>InstructaChef</Text>
         </View>
-        <View style={styles.convertSection}>
+        <View style={styles.inputSection}>
+          <FontAwesome
+            name="search"
+            size={30}
+            color={"#5F6F52"}
+            style={styles.searchIcon}
+          />
           <TextInput
             style={styles.input}
-            placeholder="https://never-gonna-give-up-video"
-            // You'll want to update this state with the actual value
+            placeholder={"https://never-gonna-give-up-video"}
           />
-          <TouchableHighlight onPress={handleConvertPress}>
-            <Text style={{fontFamily: "KaiseiDecol_400Regular"}}>Convert</Text>
+        </View>
+        <View style={{ alignItems: "center", marginTop: 20 }}>
+          <TouchableHighlight
+            style={styles.convertSection}
+            onPress={handleConvertPress}
+            underlayColor="#DDDDDD"
+          >
+            <Text style={styles.convertText}>Convert</Text>
           </TouchableHighlight>
         </View>
+        <Text style={styles.recentText}>Recent</Text>
         <View style={styles.recentRecipes}>
           <View style={styles.recipeCard}>
             <Image
               style={styles.image}
               source={require("./../../assets/pasta.png")}
             />
-            <Text style={styles.recipeTitle}>GIGI HADID'S FAMOUS Pasta</Text>
-            <Text style={styles.time}>25min</Text>
+            <Text style={styles.recipeTitle}>
+              GIGI HADID'S FAMOUS Pasta {"                          "}
+              <FontAwesome name="clock-o" size={20} /> 20min
+            </Text>
           </View>
           <View style={styles.recipeCard}>
             <Image
               style={styles.image}
               source={require("./../../assets/burger.png")}
             />
-            <Text style={styles.recipeTitle}>Classic Cheese Burger</Text>
-            <Text style={styles.time}>30min</Text>
+            <Text style={styles.recipeTitle}>
+              Classic Cheese Burger {"                                        "}
+              <FontAwesome name="clock-o" size={20} /> 30min
+            </Text>
           </View>
         </View>
-      </ScrollView>
+      </View>
     );
   }
-};
+}
 export default HomePage;
 
 const customTextProps = {
@@ -81,7 +98,6 @@ const styles = StyleSheet.create({
   container: {
     flex: 1,
     padding: 10,
-    backgroundColor: "#fff",
   },
   header: {
     marginTop: 80,
@@ -90,48 +106,80 @@ const styles = StyleSheet.create({
   },
   headerText: {
     fontFamily: "KaiseiDecol_700Bold",
+    color: "#604933",
     fontSize: 80,
   },
   convertSection: {
     alignItems: "center",
-    marginVertical: 20,
-  },
-  input: {
-    height: 40,
-    borderColor: "gray",
-    borderWidth: 1,
-    padding: 10,
-    width: "80%",
-    marginBottom: 10,
-  },
-  recentRecipes: {
-    fontFamily: "KaiseiDecol_400Regular",
-    flexDirection: "row",
-    justifyContent: "space-between",
-  },
-  recipeCard: {
-    width: "48%",
-    shadowColor: "#000",
+    borderWidth: 2,
+    borderColor: "#A9B388",
+    width: "20%",
+    shadowColor: '#000',
     shadowOffset: { width: 0, height: 1 },
     shadowOpacity: 0.22,
     shadowRadius: 2.22,
+    borderRadius: 10,
+  },
+  convertText: {
+    fontFamily: "KaiseiDecol_700Bold",
+    fontSize: 32,
+    margin: 5,
+    paddingHorizontal: 30,
+    color: "#5F6F52",
+  },
+  inputSection: {
+    flexDirection: "row",
+    justifyContent: "space-between",
+    marginTop: 30,
+    alignItems: "center",
+    borderColor: "#A9B388",
+    borderWidth: 2,
+    paddingVertical: 5,
+    marginHorizontal: 100,
+    shadowColor: '#000',
+    shadowOffset: { width: 0, height: 1 },
+    shadowOpacity: 0.22,
+    shadowRadius: 2.22,
+    borderRadius: 10,
+  },
+  input: {
+    height: 40,
+    width: "90%",
+    marginBottom: 10,
+    fontSize: 30,
+    fontFamily: "KaiseiDecol_400Regular",
+  },
+  recentRecipes: {
+    fontFamily: "KaiseiDecol_700Bold",
+    flexDirection: "row",
+    justifyContent: "space-evenly",
+  },
+  recipeCard: {
+    width: "48%",
     elevation: 3,
-    borderRadius: 4,
+    borderRadius: 10,
     overflow: "hidden",
   },
   image: {
     width: "100%",
-    height: 200,
+    height: 250,
   },
   recipeTitle: {
-    fontFamily: "KaiseiDecol_400Regular",
+    fontFamily: "KaiseiDecol_700Bold",
     fontWeight: "bold",
+    color: "#FFF",
+    fontSize: 20,
     padding: 10,
-    backgroundColor: "#f9f9f9",
+    backgroundColor: "#B99470",
   },
-  time: {
-    fontFamily: "KaiseiDecol_400Regular",
-    padding: 10,
-    color: "#666",
+  recentText: {
+    paddingHorizontal: "1.5%",
+    fontFamily: "KaiseiDecol_700Bold",
+    fontSize: 30,
+    color: "#604933",
+    marginVertical: 10,
+  },
+  searchIcon: {
+    paddingLeft: 20,
   },
 });
